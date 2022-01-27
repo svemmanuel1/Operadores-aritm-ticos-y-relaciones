d538d453adf1718a3965c3aab1576f78621ba43f
Emmanuel Vazquez <87283671+svemmanuel1@users.noreply.github.com>
2022-01-27 09:24:01 -0800

    Add files via upload

---
+++ Operador relcional.cpp
@@ -0,0 +1,18 @@
+/////////////////////////////////////////////////////////////////
+/// Operadores aritméticos y relaciones
+/// Emmanuel Suarez
+/// Operadores relacionales 
+
+
+#include <iostream>
+using namespace std;
+
+int a=10;
+int b=5;
+bool comparacion = a > b; //Verdadero 1 Falso 0 // Menor que Mayor que
+
+int main()
+{
+	cout<<comparacion;
+}
+
